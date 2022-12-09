336e258a59e37220821fae3d132c8f6588c249d0
zionokabs <Okabiechijioke@gamil.com>
2022-12-08 17:14:13 -0800

    1st C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**                                                                                                                                     
+ *
+ *  * main - Entry                                                                                                                         
+ *
+ *   * Return: Always 0                                                                                                                     
+ *
+ *    */                                                                                                                                     
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+	        puts("\"Programming is like building a multi lingual puzzle);                                                            
+
+			        return (0);
+
+}
