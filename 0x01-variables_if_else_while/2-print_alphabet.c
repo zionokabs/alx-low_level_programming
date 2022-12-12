c5fc0f7cdc876e464f15225096c238afa2c99e7d
zionokabs <okabiechijioke@gmail.com>
2022-12-12 01:10:28 +0000

    variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,37 +2,23 @@
 #include <stdlib.h>
 
 
-
 /**
-
  * main - returns alphabets
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-
 {
 
 	int ch = 'a';
 
-
-
 	while (ch <= 'z')
 
 	{
-
 		putchar(ch);
-
 		ch++;
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
