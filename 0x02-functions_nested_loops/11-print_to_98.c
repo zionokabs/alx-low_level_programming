716325a2d52a0ca6032bc7c53c215fafb5e0c983
zionokabs <Okabiechijioke@gamil.com>
2022-12-14 06:06:10 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - a function that prints all natural numbers from n to 98.
  * @n: parameter of the function
@@ -6,61 +7,17 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		while (n <= 98)
-		{
-			if (n >= 10 || (n <= -10 && n > -100))
-			{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			if (n != 98)
-			{
-			_putchar(',');
-			_putchar(' ');
-			}
-			}
-			else if ((n > 0 && n < 9) || (n < 0 && n > -10))
-			{
-				_putchar(n + '0');
-				if (n != 98)
-				{
-				_putchar(',');
-				_putchar(' ');
-				}
-			}
-			n++;
-		}
-		_putchar('\n');
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
-	else if (n > 98)
+	else
 	{
 		while (n >= 98)
-		{
-			if (n >= 100)
-			{
-			_putchar((n / 100) + '0');
-			_putchar((n % 100) / 10 + '0');
-			_putchar((n % 10) + '0');
-			if (n != 98)
-			{
-			_putchar(',');
-			_putchar(' ');
-			}
-			}
-			if (n < 100)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				if (n != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			n--;
-		}
-		_putchar('\n');
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	return;
+
 }
