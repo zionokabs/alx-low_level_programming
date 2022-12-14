91ae5cda3aa38f40dbaf709ba90f85b5b951eb0d
zionokabs <Okabiechijioke@gamil.com>
2022-12-14 03:48:21 -0800

    Nested loo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * main - Entry point
  * Description: A function that prints _putchar
  * Return: Allow success
  */
 int main(void)
-{	
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
