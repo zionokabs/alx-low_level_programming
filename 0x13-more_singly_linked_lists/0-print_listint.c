ba273fea9c101e6fa4babb7d63582dd969c1c6f4
zionokabs <Okabiechijioke@gamil.com>
2023-01-30 16:13:03 -0800

    Singly extended

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - prints all elements of a list_t list
+ * @h: singly linked list elements
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t x;
+
+	for (x = 0; h; x++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (x);
+}
